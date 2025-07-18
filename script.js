686e9f29f98106edfa3a805da1502d516699cb71
Valeria-Bazan-Molero <valeria.bazan.molero@gmail.com>
2025-07-18 19:18:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ menuToggle.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
 
-// Opcional: cerrar menú al hacer click fuera (si quieres)
-document.addEventListener('click', (e) => {
-  if (!menu.contains(e.target) && !menuToggle.contains(e.target)) {
-    menu.classList.remove('show');
-  }
-});
-
 // Carrusel automático
 const carouselImages = document.querySelectorAll('.carousel img');
 let currentIndex = 0;
@@ -22,4 +15,4 @@ function showNextImage() {
   carouselImages[currentIndex].classList.add('active');
 }
 
-setInterval(showNextImage, 3000); // cambia cada 3 segundos
+setInterval(showNextImage, 3000); // Cambia cada 3 segundos
